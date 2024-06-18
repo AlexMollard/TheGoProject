5dce6bb3b623128da9298908935fa4f47768ae05
Alex Mollard <alex.mollard@mail.com>
2024-06-18 20:09:23 +1000

    Further completed the hello world topic

--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -3,9 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Alex")
-	want := "Hello, Alex"
+	t.Run("saying hello to people", func(t *testing.T) {
 
+		got := Hello("Alex")
+		want := "Hello, Alex"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello when an empty string is supplied", func(t *testing.T) {
+
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
